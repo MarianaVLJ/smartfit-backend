6f3d9ebd2384889cabcce393f30711542d41dc46
Claudia Agostinelli <cagostinelli@unsa.edu.pe>
2026-07-21 17:25:18 -0500

    feat(suscripciones): implementa actualizacion y eliminacion de suscripciones

--- src/infrastructure/repositories/suscripcion_repository.py
+++ src/infrastructure/repositories/suscripcion_repository.py
@@ -5,6 +5,7 @@ from src.infrastructure.database import db
 class SuscripcionRepository:
 
     def crear_suscripcion(self, datos):
+
         nueva_suscripcion = Suscripcion(
             dni=datos["dni"],
             plan=datos["plan"],
@@ -18,7 +19,38 @@ class SuscripcionRepository:
 
 
     def obtener_suscripciones(self):
+
         return Suscripcion.query.all()
+
+
+
+    def actualizar_suscripcion(self, id, datos):
+
+        suscripcion = Suscripcion.query.get(id)
+
+        if not suscripcion:
+            return None
+
+        suscripcion.plan = datos["plan"]
+
+        db.session.commit()
+
+        return suscripcion
+
+
+
+    def eliminar_suscripcion(self, id):
+
+        suscripcion = Suscripcion.query.get(id)
+
+        if not suscripcion:
+            return None
+
+        db.session.delete(suscripcion)
+        db.session.commit()
+
+        return suscripcion
+
 
 
     def crear_contingencia(self, datos):
@@ -34,5 +66,7 @@ class SuscripcionRepository:
         return nueva_contingencia
 
 
+
     def obtener_contingencias(self):
+
         return Contingencia.query.all()
